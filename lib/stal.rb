3fef09d3ceb5ccf1520c343528c3424d37f8b66c
Michel Martens <michel@soveran.com>
2016-12-16 14:59:04 +0100

    Use a Lua script

--- lib/stal.rb
+++ lib/stal.rb
@@ -1,85 +1,23 @@
 # encoding: UTF-8
 
+require "json"
 require "redic"
 
 module Stal
-  class InvalidCommand < ArgumentError; end
-
-  COMMANDS = {
-    :SDIFF  => 'SDIFFSTORE',
-    :SINTER => 'SINTERSTORE',
-    :SUNION => 'SUNIONSTORE',
-  }
-
-  def self.command(term)
-    COMMANDS.fetch(term) do
-      raise(InvalidCommand, term)
-    end
-  end
-
-  # Compile expression into Redis commands
-  def self.compile(expr, ids, ops)
-    expr.map do |item|
-      if Array === item
-        convert(item, ids, ops)
-      else
-        item
-      end
-    end
-  end
-
-  # Transform :SDIFF, :SINTER and :SUNION commands
-  # into SDIFFSTORE, SINTERSTORE and SUNIONSTORE.
-  def self.convert(expr, ids, ops)
-    head, *tail = expr
-
-    # Key where partial results will be stored
-    id = sprintf("stal:%s", ids.size)
-
-    # Keep a reference to clean it up later
-    ids.push(id)
-
-    # Translate into command and destination key
-    op = [command(head), id]
-
-    # Compile the rest recursively
-    op.concat(compile(tail, ids, ops))
-
-    # Append the outermost operation
-    ops.push(op)
-
-    return id
-  end
-
-  # Return commands without any wrapping added by `solve`
-  def self.explain(expr)
-    ids = []
-    ops = []
-
-    ops.push(compile(expr, ids, ops))
-
-    return ops
-  end
+  LUA = File.expand_path("../../data/stal.lua", __FILE__)
+  SHA = "e98658a3aca397c69e8b452a73c6826cdd9f0577"
 
   # Evaluate expression `expr` in the Redis client `c`.
   def self.solve(c, expr)
-    ids = []
-    ops = []
-
-    ops.push(compile(expr, ids, ops))
-
-    if ops.one?
-      c.call(*ops[0])
-    else
-      c.queue("MULTI")
-
-      ops.each do |op|
-        c.queue(*op)
+    begin
+      c.call!("EVALSHA", SHA, 0, JSON.dump(expr))
+    rescue RuntimeError
+      if $!.message["NOSCRIPT"]
+        c.call!("SCRIPT", "LOAD", File.read(LUA))
+        c.call!("EVALSHA", SHA, 0, JSON.dump(expr))
+      else
+        raise $!
       end
-
-      c.queue("DEL", *ids)
-      c.queue("EXEC")
-      c.commit[-1][-2]
     end
   end
 end
--- test/all.rb
+++ test/all.rb
@@ -3,6 +3,7 @@ require_relative "helper"
 setup do
   Redic.new.tap do |c|
     c.call("FLUSHDB")
+    c.call("SCRIPT", "FLUSH")
     c.call("SADD", "foo", "a", "b", "c")
     c.call("SADD", "bar", "b", "c", "d")
     c.call("SADD", "baz", "c", "d", "e")
@@ -13,36 +14,17 @@ end
 test do |c|
 
   # Example expression
-  expr = ["SUNION", "qux", [:SDIFF, [:SINTER, "foo", "bar"], "baz"]]
-
-  assert_equal ["b", "x", "y", "z"], Stal.solve(c, expr).sort
-
-  # Commands in sub expressions must be symbols
   expr = ["SUNION", "qux", ["SDIFF", ["SINTER", "foo", "bar"], "baz"]]
 
-  assert_raise(Stal::InvalidCommand) do
-    Stal.solve(c, expr)
-  end
+  assert_equal ["b", "x", "y", "z"], Stal.solve(c, expr).sort
 
   # Commands without sub expressions also work
   expr = ["SINTER", "foo", "bar"]
 
   assert_equal ["b", "c"], Stal.solve(c, expr).sort
 
-  # Only :SUNION, :SDIFF and :SINTER are supported in sub expressions
-  expr = ["SUNION", ["DEL", "foo"]]
-
-  assert_raise(Stal::InvalidCommand) do
-    Stal.solve(c, expr)
-  end
-
   # Verify there's no keyspace pollution
   assert_equal ["bar", "baz", "foo", "qux"], c.call("KEYS", "*").sort
 
-  expr = ["SCARD", [:SINTER, "foo", "bar"]]
-
-  # Explain returns an array of Redis commands
-  expected = [["SINTERSTORE", "stal:0", "foo", "bar"], ["SCARD", "stal:0"]]
-
-  assert_equal expected, Stal.explain(expr)
+  expr = ["SCARD", ["SINTER", "foo", "bar"]]
 end
